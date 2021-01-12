71d30872d29d012b4fa0175e6ddb67ba22fbb555
LordSir101 <49959269+LordSir101@users.noreply.github.com>
2021-01-11 19:24:27 -0500

    Added check for query rate limits

--- LOL Tilt Protection/protect.py
+++ LOL Tilt Protection/protect.py
@@ -2,37 +2,45 @@ from riotwatcher import LolWatcher, ApiError
 import psutil
 import time
 
+timeInterval = 10
 name = input("Please enter your LOL username\n")
 
+
+# golbal variables
+api_key = 'key'
+watcher = LolWatcher(api_key)
+my_region = 'na1'
+
+# summoner details
+me = watcher.summoner.by_name(my_region, name)
+#print(me)
+
+print("\n---Ranked---\n")
+
+ranked_stats = watcher.league.by_summoner(my_region, me['id'])
+print(ranked_stats)
+
 while(True):
-    # golbal variables
-    api_key = 'RGAPI-d15ae015-04bb-4cb8-b3db-cb41c91077bb'
-    watcher = LolWatcher(api_key)
-    my_region = 'na1'
-
-    # summoner details
-    me = watcher.summoner.by_name(my_region, name)
-    #print(me)
-
-    print("\n---Ranked---\n")
-
-    ranked_stats = watcher.league.by_summoner(my_region, me['id'])
-    print(ranked_stats)
-
     print("\n---Matches---\n")
 
     # get details of most recent matches
-    matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
-
+    try:
+        matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
+    except:
+        time.sleep(5)
+        continue
     # last two matches played
     last_match = matches['matches'][0]
     second_last_match = matches['matches'][1]
 
     # get champion details of each match
     # this will get data for all players
-    match_detail = watcher.match.by_id(my_region, last_match['gameId'])
-    match_detail2 = watcher.match.by_id(my_region, second_last_match['gameId'])
-
+    try:
+        match_detail = watcher.match.by_id(my_region, last_match['gameId'])
+        match_detail2 = watcher.match.by_id(my_region, second_last_match['gameId'])
+    except:
+        time.sleep(5)
+        continue
     # Find the difference between games in mins
     # Riot API has timestamps in milli
     timeDiff = int(last_match['timestamp']) - int(second_last_match['timestamp'])
@@ -44,7 +52,7 @@ while(True):
 
     if(timeLastPlayed > 120):
         print("The last game played was more than 2 hours ago")
-        time.sleep(5)
+        time.sleep(timeInterval)
         continue
 
     # check if both the matches were ranked and were started within 2 hours of each other
@@ -83,4 +91,4 @@ while(True):
         print("The last two matches were not ranked")
 
     # check matches every 5 seconds
-    time.sleep(5)
+    time.sleep(timeInterval)
